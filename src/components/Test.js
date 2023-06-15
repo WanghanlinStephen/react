5fb9e5c78a2845da399e2372a494f2b4b80893d4
汪翰林 <wanghanlin@wanghanlindeMacBook-Pro.local>
2023-06-15 09:33:40 -0700

    test

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,146 +1,45 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
-import { Table } from "react-bootstrap";
-import DownIcon from '../assets/down.png';
+import { Col, Row, Breadcrumb, Tabs, Tab } from 'react-bootstrap';
+import ManageAccount from './ManageAccount';
+import ExistingAccounts from './ExistingAccounts';
 
-var AccountTable = createReactClass({
+var Home = createReactClass({
   getInitialState: function() {
     return {
-      accounts: [
-        {
-          id: 1,
-          memberName: 'Jack',
-          accountType: 'basic_auth_user',
-          posixGroup: 'hweng',
-          readOnly: true,
-          requestedBy: 'xuanw',
-          requestedOn: '2012-12-18',
-          odinMaterialSet: 'Ok'
-        },
-        {
-          id: 2,
-          memberName: 'Bob',
-          accountType: 'basic_auth_user',
-          posixGroup: 'hweng',
-          readOnly: false,
-          requestedBy: 'xuanw',
-          requestedOn: '2012-12-18',
-          odinMaterialSet: 'Ok'
-        }
-      ]
+      tabName: 'manage-account'
     };
   },
 
   render: function() {
     return (
-      <div>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>
-                <input type={'checkbox'}/>
-              </th>
-              <th>
-                Member Name
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-              <th>
-                Account Type
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-              <th>
-                POSIX Group
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-              <th>
-                Read-only
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-              <th>
-                Requested By
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-              <th>
-                Requested On
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-              <th>
-                Odin Material Set
-                <img src={DownIcon} width={15}
-                  style={{
-                    marginLeft: '10px',
-                    cursor: 'pointer'
-                  }}
-                />
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.accounts.map(account => {
-              return (
-                <tr key={account.id}>
-                  <td>
-                    <input type={'checkbox'}/>
-                  </td>
-                  <td>
-                    {account.memberName}
-                  </td>
-                  <td>
-                    {account.accountType}
-                  </td>
-                  <td>
-                    {account.posixGroup}
-                  </td>
-                  <td>
-                    {account.readOnly ? 'Yes' : 'No'}
-                  </td>
-                  <td>
-                    {account.requestedBy}
-                  </td>
-                  <td>
-                    {account.requestedOn}
-                  </td>
-                  <td>
-                    {account.odinMaterialSet}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
+      <Row>
+        <Col xs={1}>
+          {/* <div style={{borderRight: '1px solid grey'}}>
+            <NavBar />
+          </div> */}
+        </Col>
+        <Col xs={10}>
+          <div style={{marginTop: '20px'}}>
+            <Breadcrumb>
+              <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+              <Breadcrumb.Item>Manage Accounts</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <h2 style={{marginBottom: '40px'}}>Manage Account</h2>
+          <Tabs defaultActiveKey={this.state.tabName}>
+            <Tab eventKey={'manage-account'} title={'Create a new account'}>
+              <ManageAccount />
+            </Tab>
+            <Tab eventKey={'existing-accounts'} title={'Existing accounts'}>
+              <ExistingAccounts />
+            </Tab>
+          </Tabs>
+        </Col>
+        <Col xs={1}></Col>
+      </Row>
     );
   }
 });
 
-export default AccountTable;
+export default Home;
